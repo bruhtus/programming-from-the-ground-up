09696188576a9890e30314153b2029061391b97d
Robertus Chris <hibruhtus@gmail.com>
2026-09-07 14:30:48 +0700

    common: add linux x86-64 constants
    Contains system call number and register size for x86-64 assembly
    instructions. This system call number and register size is different
    from x86-32 or x86 (32-bit), hence why put this in different file.

---
+++ common/linux-x86-64.s
@@ -0,0 +1,8 @@
+.equ SYS_EXIT, 60
+.equ SYS_READ, 0
+.equ SYS_WRITE, 1
+.equ SYS_OPEN, 2
+.equ SYS_CLOSE, 3
+.equ SYS_BRK, 12
+
+.equ REG_SIZE, 8 # 8 bytes (1 bytes = 8 bits, 8 bytes = 64 bits (8 * 8))
